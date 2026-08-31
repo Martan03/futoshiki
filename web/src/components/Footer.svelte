f1c5b8f97c8c7e88b81e205223f757b6a7b0d3ff
Martan03 <martinslezak03@gmail.com>
2026-08-31 23:02:47 +0200

    Add rules and footer

---
+++ web/src/components/Footer.svelte
@@ -0,0 +1,38 @@
+<footer>
+    <a href="https://github.com/Martan03/futoshiki" target="_blank">
+        Source Code
+    </a>
+    <span class="divider">•</span>
+    <a href="https://github.com/Martan03" target="_blank">GitHub Profile</a>
+    <span class="divider">•</span>
+    <a href="https://martan03.github.io/" target="_blank">Portfolio</a>
+</footer>
+
+<style>
+    footer {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        gap: 1rem;
+        margin-top: auto;
+        width: 100%;
+        padding: 1.2rem 0 1.2rem 0;
+        border-top: 1px solid var(--border);
+        font-size: 0.9rem;
+    }
+
+    footer a {
+        color: var(--text-h);
+        text-decoration: none;
+        transition: color 0.1s;
+    }
+
+    footer a:hover {
+        color: var(--accent-border);
+    }
+
+    .divider {
+        color: var(--border);
+        user-select: none;
+    }
+</style>
